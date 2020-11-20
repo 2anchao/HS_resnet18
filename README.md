47c5146daf1b78f4f3fa17ca873f871ffbc9e908
安超 <anchao@anchaodeMacBook-Pro.local>
2020-11-20 18:16:17 +0800

    hsresnet18

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # HS_resnet18
-HS_resnet18
+
+## 参考
+# [paper](https://arxiv.org/abs/2010.07621)
+# [HS-ResNet]https://github.com/bobo0810/HS-ResNet
+## 环境
+- python
+- pytorch
+- ubuntu
+
+## 训练
+- SGD
+- cross_entropy
+- base_lr:0.1 (cosine learning rate)
+
+## 模型核心
+
+## 其它
+- 基本上都是参考代码：https://github.com/bobo0810/HS-ResNet 
+- 修改得到了HS_resnet18
+- HS_resnet18的Imagenet测试集正确率为：73.5
+- 模型位置，可作为检测、分割等视觉任务的预训练模型，模型位置：
+- 训练结果并不理想，模型的速度和存储大小都比较大，正确率也低于我训练得到的resnest18（后续会开源，resnest18的Imagenet测试集正确率为74.5）
+- 等待百度开源代码
+- 感觉模型的创新点并不是很强，和res2net很像，都是分组级联（分组减少参数、级联增加感受野可能性，特征交互融合、细粒度特征提取）
+
---
+++ hsresnet.py
@@ -0,0 +1,296 @@
+import torch 
+import torch.nn as nn
+from typing import Type, Any, Callable, Union, List, Optional
+from torch import Tensor
+
+class HSBlock(nn.Module):
+    def __init__(self, in_channel: int, split_num: int=5):
+        '''
+        in_channel: input_channel
+        split_num: splited group number
+        '''
+        super(HSBlock, self).__init__()
+        self.split_num = split_num
+        self.module_list = nn.ModuleList()
+        if in_channel % split_num == 0:
+            split_channel, last_channel = in_channel//split_num, in_channel//split_num
+        else:
+            split_channel, last_channel = in_channel//split_num+1, in_channel - (in_channel//split_num+1)*(split_num-1)
+        split_again_channels = 0
+        self.module_list.append(None)
+        for i in range(1, self.split_num):
+            if i == 1:
+                concat_channels = split_channel
+                split_again_channels = concat_channels//2
+            elif i == self.split_num - 1:
+                concat_channels = last_channel + split_again_channels
+            else:
+                concat_channels = split_channel + split_again_channels
+                split_again_channels = concat_channels//2
+
+            self.module_list.append(self.conv_bn_relu(in_ch=concat_channels, out_ch=concat_channels))
+        self.initialize_weights()
+
+    def conv_bn_relu(self, in_ch, out_ch, kernel_size=3, stride=1, padding=1):
+        conv_bn_relu = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size, stride, padding),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+        return conv_bn_relu
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+    def forward(self, input_x):
+        x = list(torch.chunk(input_x, self.split_num, dim=1))
+        for i in range(1, len(self.module_list)):
+            y = self.module_list[i](x[i])
+            if i == len(self.module_list) - 1:
+                x[0] = torch.cat((x[0], y), 1)
+            else:
+                y1, y2 = y.chunk(chunks=2, dim=1)
+                x[0] = torch.cat((x[0], y1), 1)
+                x[i + 1] = torch.cat((x[i + 1], y2), 1)
+        return x[0]
+
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self,
+                 inplanes,
+                 planes,
+                 stride=1,
+                 downsample=None,
+                 groups=1,
+                 base_width=64,
+                 dilation=1,
+                 norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError(
+                'BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError(
+                "Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        return out
+
+class BasicBlock(nn.Module):
+    expansion: int = 1
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.hsblock = HSBlock(planes)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        # use HSBlock here
+        out = self.hsblock(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(
+        self,
+        block,
+        layers: List[int],
+        num_classes: int = 1000,
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation: Optional[List[bool]] = None,
+        norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        # classfiler                               
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+
+    def _make_layer(self, block, planes: int, blocks: int,
+                    stride: int = 1, dilate: bool = False) -> nn.Sequential:
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x: Tensor) -> Tensor:
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        stage4 = self.layer1(x)
+        stage8 = self.layer2(stage4)
+        stage16 = self.layer3(stage8)
+        stage32 = self.layer4(stage16)
+        out = self.avgpool(stage32)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+
+        return out
+        
+class HS_ResNet18(ResNet):
+    def __init__(self, num_classes=1000, **kwargs):
+        super(HS_ResNet18, self).__init__(
+                                       num_classes=num_classes,
+                                       block=BasicBlock,
+                                       layers=[2, 2, 2, 2],
+                                       **kwargs)
+        self.return_features_num_channels = [128, 256, 512]
+
+if __name__ == "__main__":
+    import time
+    test_input = torch.ones(2, 3, 960, 544).cuda()
+    model = HS_ResNet18(num_classes=100).cuda()
+    start = time.time()
+    out = model(test_input)
+    end = time.time()
+    print("cost time:", end-start)
+    print(out.shape)
+
